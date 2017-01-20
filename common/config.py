48b7a5e9f60e36f3170e4aacc8cab2ffd676e724
opodame <opodame@gmail.com>
2017-01-20 05:33:40 +0000

    fixed debug config error

--- common/config.py
+++ common/config.py
@@ -3,4 +3,4 @@ All custom configurations for the flask app
 '''
 from os import environ as env
 
-DEBUG = True if str(env.get('FLASK_DEBUG')) == '1' else False
+DEBUG = True if str(env.get('FLASK_DEBUG', '0')) == '1' else False
